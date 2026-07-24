f47a21eacb9fdb685f31a931cdcaeefbd465205b
Ayush Dubey <ayushdubeyit@gmail.com>
2026-07-24 11:53:01 +0530

    Implemented gateway security config

--- GraphQL-Gateway/src/main/java/com/ayush/subscription/gateway/config/WebClientConfig.java
+++ GraphQL-Gateway/src/main/java/com/ayush/subscription/gateway/config/WebClientConfig.java
@@ -1,9 +1,15 @@
 package com.ayush.subscription.gateway.config;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -12,6 +18,34 @@ public class WebClientConfig {
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        return WebClient.builder()
+                .filter(authorizationHeaderForwardingFilter());
+    }
+
+    private ExchangeFilterFunction authorizationHeaderForwardingFilter() {
+        return (request, next) -> {
+            ServletRequestAttributes attributes = (ServletRequestAttributes)
+                    RequestContextHolder.getRequestAttributes();
+
+            if (attributes == null) {
+                return next.exchange(request);
+            }
+
+            HttpServletRequest servletRequest = attributes.getRequest();
+            String authorizationHeader = servletRequest
+                    .getHeader(HttpHeaders.AUTHORIZATION);
+
+            if (authorizationHeader == null) {
+                return next.exchange(request);
+            }
+
+            ClientRequest forwardedRequest = ClientRequest.from(request)
+                    .headers(headers -> headers.set(
+                            HttpHeaders.AUTHORIZATION,
+                            authorizationHeader))
+                    .build();
+
+            return next.exchange(forwardedRequest);
+        };
     }
 }
