d0e4b306a4ff2f732db749aa04f596a10007d42e
Ayush Dubey <ayushdubeyit@gmail.com>
2026-07-25 16:27:24 +0530

    Implemented automatic payment renewal  schedular

--- subscription-service/src/main/java/com/ayush/subscription/subscription/SubscriptionServiceApplication.java
+++ subscription-service/src/main/java/com/ayush/subscription/subscription/SubscriptionServiceApplication.java
@@ -3,12 +3,14 @@ package com.ayush.subscription.subscription;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 
 @EnableCaching
 @SpringBootApplication
+@EnableScheduling
 public class SubscriptionServiceApplication {
 
 	public static void main(String[] args) {
